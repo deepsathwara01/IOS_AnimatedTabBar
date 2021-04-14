f4d5b9530fe09559eb4078c4af91fb2447273006
Deep Sathwara <72172951+deepsathwara01@users.noreply.github.com>
2021-04-14 01:54:11 -0400

    Add files via upload

---
+++ AnimatedBottomSlider/AnimatedBottomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBottomSliderApp.swift
+//  AnimatedBottomSlider
+//
+//  Created by Deep Sathwara on 2021-03-17.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBottomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
